b56ddff5a858c1662e18db8dddbccd5407e76575
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-24 12:51:28 +0600

    first commit

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,31 @@
-import express, { Request, Response } from 'express'
-const app = express()
+import cors from 'cors'
+import express, { Application, Request, Response } from 'express'
 
+const app: Application = express()
+
+// parsers
+app.use(express.json())
+app.use(
+  cors({
+    origin: ['http://localhost:3000'],
+    credentials: true
+  })
+)
+
+// module routes
+// app.use('/api', router)
+
+// test routes
 app.get('/', (req: Request, res: Response) => {
-  const a = 'bolo'
+  const a = 'Hello world'
 
   res.send(a)
 })
+
+// global error handler middleware
+// app.use(globalErrorHandler)
+
+// not found route middleware
+// app.use(notFound)
 
 export default app
