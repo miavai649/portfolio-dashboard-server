94e211c0e2116b2ad30d49bc8baa124f432e2ce2
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-25 19:35:52 +0600

    projects modal created

--- src/app/modules/projects/projects.interface.ts
+++ src/app/modules/projects/projects.interface.ts
@@ -1,4 +1,4 @@
-export type Project = {
+export type TProject = {
   title: string
   description: string
   technologies: string[]
---
+++ src/app/modules/projects/projects.model.ts
@@ -0,0 +1,23 @@
+import { model, Schema } from 'mongoose'
+import { TProject } from './projects.interface'
+
+const projectSchema = new Schema<TProject>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    technologies: {
+      type: [String],
+      required: true
+    }
+  },
+  { timestamps: true }
+)
+
+export const Projects = model<TProject>('Projects', projectSchema)
