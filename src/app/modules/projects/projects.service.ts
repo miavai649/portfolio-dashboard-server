ec50db999d2ebabdd81caa68b14bfcb42ed21657
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-25 21:40:35 +0600

    bodyDataParsing utlis functionality modified

--- src/app/modules/projects/projects.service.ts
+++ src/app/modules/projects/projects.service.ts
@@ -1,14 +1,14 @@
-import { TImageFiles } from "../../interfaces/file.interface";
-import { TProject } from "./projects.interface";
-import { Projects } from "./projects.model";
+import { TImageFiles } from '../../interfaces/file.interface'
+import { TProject } from './projects.interface'
+import { Projects } from './projects.model'
 
 const createProjectIntoDb = async (payload: TProject, images: TImageFiles) => {
-  payload.images = images.postImages?.map((image) => image?.path);
+  payload.images = images.projectImages?.map((image) => image?.path)
 
-  const post = await Projects.create(payload);
-  return post;
-};
+  const post = await Projects.create(payload)
+  return post
+}
 
 export const ProjectServices = {
-  createProjectIntoDb,
-};
+  createProjectIntoDb
+}
