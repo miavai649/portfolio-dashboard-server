d6f76cf061feee1284a00f522e944a1717c480cd
miavai649 <mahmudulhaquenoor@gmail.com>
2024-10-27 18:31:46 +0600

    final commit

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,10 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: [
+      "http://localhost:3000",
+      "https://noor-portfolio-brown.vercel.app/",
+    ],
     credentials: true,
   }),
 );
